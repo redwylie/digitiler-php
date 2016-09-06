cf295bcd2e6547a1a6533da5c98069714894848b
Mark Wylie <mwylie@mgh.harvard.edu>
2016-09-06 15:36:59 -0400

    added twig

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $loader = new Twig_Loader_Filesystem('views');
 
 $twig = new Twig_Environment($loader, array(
   'cache' => false,
+  'auto_reload' => true,
 ));
 
 $template = $twig->loadTemplate('index.html');
